19537e102795268faca6c267502cc22bcea49196
sanchit237 <sanchitnemlekar8@gmail.com>
2022-06-20 22:03:34 +0530

    Variables and data types

---
+++ 01. Getting started with JS/02Basics/variables.js
@@ -0,0 +1,13 @@
+var fullName = "Sanchit Nemlekar";
+
+var courseName = "Javascript";
+
+var isLoggedIn = true;
+
+var loggedCount = 34;
+
+var paymentMode;
+paymentMode = "Cash";
+
+console.log(paymentMode);
+
