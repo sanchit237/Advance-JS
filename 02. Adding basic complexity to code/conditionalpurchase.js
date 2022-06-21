f970b08c700e7200188636aaf006f30b5077368b
sanchit237 <sanchitnemlekar8@gmail.com>
2022-06-21 22:14:55 +0530

    Conditional purchase

---
+++ 02. Adding basic complexity to code/conditionalpurchase.js
@@ -0,0 +1,10 @@
+var isLoggedIn = true;
+var isEmailVerified = true;
+var cardInfo = true;
+
+if (isLoggedIn && isEmailVerified && cardInfo) {
+	console.log("Login successful");
+}
+else {
+	console.log("Login unsuccessful");
+}
