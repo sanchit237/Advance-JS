a3ccc5875f6921c78f0c5b00817225b74f79e518
sanchit237 <sanchitnemlekar8@gmail.com>
2022-06-21 22:02:47 +0530

    Conditional logic

---
+++ 02. Adding basic complexity to code/conditional.js
@@ -0,0 +1,12 @@
+var temperature;
+
+// temperature = 30;
+temperature = 31;
+temperature = 29;
+
+if (temperature < 30){
+	console.log("The temperature is moderate");
+}
+else{
+	console.log("The temperature is cold");
+}
