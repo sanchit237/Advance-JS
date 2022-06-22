bd1176a3e977be409ca3559d7695c7c95967f416
sanchit237 <sanchitnemlekar8@gmail.com>
2022-06-22 09:18:20 +0530

    Ternary operator

---
+++ 02. Adding basic complexity to code/conditionalsignout.js
@@ -0,0 +1,10 @@
+var authenticated = true;
+
+// if (authenticated){
+// 	console.log("Show Signout Button");
+// }
+// else {
+// 	console.log("Show Login Button");
+// }
+
+authenticated ? console.log("Show Signout button") : console.log("Show login button");
