ea4ede824f4e2f4d662d71e900c10780750771e8
sanchit237 <sanchitnemlekar8@gmail.com>
2022-06-26 22:15:39 +0530

    Arrays

---
+++ 04. Arrays and loops/array.js
@@ -0,0 +1,11 @@
+var countries = ['India','Usa'];
+
+console.log(Array.from('hello'));
+
+console.log(Array.isArray(countries));
+
+console.log(countries.length);
+
+//Refer to https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array.
+
+
