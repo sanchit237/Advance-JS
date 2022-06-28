fd6f37cfe5bf44607c571b5e792b6ac76e6bccec
sanchit237 <sanchitnemlekar8@gmail.com>
2022-06-28 22:22:47 +0530

    Array fill and filter

---
+++ 04. Arrays and loops/arrayfilter.js
@@ -0,0 +1,14 @@
+//Array filter
+const numbers = [1, 2, 3, 4];
+
+var result = numbers.filter((num) => num < 3 );
+
+console.log(result);
+
+// Array Fill
+
+var test = [2, 4, 6, 8, 10];
+
+var fillres = test.fill("k");
+
+console.log(fillres);
